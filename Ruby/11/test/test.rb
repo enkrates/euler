86d9ed0305d0f84fca516d674c58b79f6ee3eac7
William Sullivan <enkrates@gmail.com>
2009-11-28 17:31:36 -0600

    Working through #11

--- Ruby/11/test/test.rb
+++ Ruby/11/test/test.rb
@@ -5,7 +5,7 @@ require "test/unit"
 require "../code.rb"
 
 class TestCodeRb < Test::Unit::TestCase
-  def test_
+  def test_greatest_product_of_four_adjacent_numbers
     assert_equal(, )
   end
 end
