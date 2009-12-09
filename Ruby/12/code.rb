0660bed78c9c6db4673c9f3f2bd69a6d3e5d92d5
William Sullivan <enkrates@gmail.com>
2009-12-09 15:52:29 -0600

    Just keeping updated on my work on problem 12

--- Ruby/12/code.rb
+++ Ruby/12/code.rb
@@ -24,17 +24,33 @@ end
 def return_first_triangle_number_with_the_following_number_of_factors factors
   
   current_leaders = Array.new
-  current_leaders[:number_of_factors] = 0
-  current_leaders[:triangle_number] = 0
+  current_leader_number_of_factors = 0
+  current_leader_triangle_number = 0
     
   current_natural_number = 1
+  number_to_add_to_current_triangle_number = 2
   
-  while current_leaders[:number_of_factors] < factors
+  while current_leader_number_of_factors < factors
     
+    current_number_of_factors = count_factors_in_a_number(current_natural_number)
     
+    if current_number_of_factors > current_leader_number_of_factors
+      current_leader_number_of_factors = current_number_of_factors
+      current_leader_triangle_number   = current_natural_number
+      puts current_number_of_factors
+    end
     
+    current_natural_number += number_to_add_to_current_triangle_number
+    
+    number_to_add_to_current_triangle_number += 1
     
   end
   
+  return current_leader_triangle_number
+  
   
 end
+
+
+
+puts return_first_triangle_number_with_the_following_number_of_factors(500)
