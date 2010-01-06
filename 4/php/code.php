67cb1a665e31946b2e3b40b8fcbae26586253699
William Sullivan <enkrates@gmail.com>
2010-01-05 21:57:49 -0600

    I feel good about problem 4 in PHP

--- 4/php/code.php
+++ 4/php/code.php
@@ -1,9 +1,26 @@
 <?php
 
-function find_factors_of_an_integer($integer)
+function get_highest_palindrome()
 {
-	# code...
+	$palindrome_array = array();
+	
+	for ($i=100; $i < 999; $i++) { 
+		for ($j=100; $j < 999; $j++) { 
+			$product = $i * $j;
+			$product_string = strval($product);
+			
+			if ($product_string == strrev($product_string)) {
+				$palindrome_array[] = $product;
+			}
+		}
+	}
+	
+	rsort($palindrome_array);
+	
+	return $palindrome_array[0];
+
 }
 
+echo get_highest_palindrome();
 
 ?>
