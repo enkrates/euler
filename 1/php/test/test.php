8619107e96dd5cf53929869300b1da205b84aff8
William Sullivan <enkrates@gmail.com>
2010-02-25 14:25:26 -0700

    Making this test code prettier.

--- 1/php/test/test.php
+++ 1/php/test/test.php
@@ -12,11 +12,10 @@ class EulerTest extends PHPUnit_Framework_TestCase
             $this->third_number  = EulerNumber(35);
         }
 	
-	public function testEulerNumbers()
-    {
-		$this->assertEquals( 23, $this->first_number,  "We were looking for 23, but got $this->first_number" );
-		$this->assertEquals( 78, $this->second_number, "We were looking for 78, but got $this->second_number" );
-		$this->assertEquals( 258, $this->third_number,  "We were looking for 258, but got $this->third_number" );
+	public function testEulerNumbers(){
+	    $this->assertEquals( 23, $this->first_number,  "We were looking for 23, but got $this->first_number" );
+	    $this->assertEquals( 78, $this->second_number, "We were looking for 78, but got $this->second_number" );
+	    $this->assertEquals( 258, $this->third_number,  "We were looking for 258, but got $this->third_number" );
 	}
 }
 
