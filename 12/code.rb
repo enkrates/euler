c18558486427de1e8b644c6fd5f6605c58628bbc
William Sullivan <enkrates@gmail.com>
2010-09-02 20:11:09 -0500

    Solved 12 by rewriting in C++ for speed

--- 12/code.rb
+++ 12/code.rb
@@ -4,40 +4,48 @@
 
 def count_factors_in_a_number number
   
-  factors = Array.new
-  factors.push(number)
+  #factors = Array.new
+  factors = 2
+  half = number/2.0
   
-  (1..(number/2)).each do |potential|
+  #(1..(Math.sqrt(number))).each do |potential|
+   (2..half).each do |potential|
+    
+    #puts potential
     
     if number % potential == 0
       
-      factors.push(potential)
+      puts potential.to_s + " is a factor of " + number.to_s
+      
+      factors += 1
       
     end
     
   end
   
-  return factors.length
+  return factors
   
 end
 
-def return_first_triangle_number_with_the_following_number_of_factors factors
+def return_first_triangle_number_with_more_than_the_following_number_of_factors factors
   
-  current_leaders = Array.new
+  #current_leaders = Array.new
   current_leader_number_of_factors = 0
   current_leader_triangle_number = 0
     
   current_natural_number = 1
   number_to_add_to_current_triangle_number = 2
   
-  while current_leader_number_of_factors < factors
+  while current_leader_number_of_factors <= factors
+    
+    puts current_natural_number
     
     current_number_of_factors = count_factors_in_a_number(current_natural_number)
     
     if current_number_of_factors > current_leader_number_of_factors
       current_leader_number_of_factors = current_number_of_factors
       current_leader_triangle_number   = current_natural_number
-      puts current_number_of_factors
+      puts current_natural_number.to_s + " has " + current_number_of_factors.to_s
     end
     
     current_natural_number += number_to_add_to_current_triangle_number
@@ -53,4 +61,4 @@ end
 
 
 
-puts return_first_triangle_number_with_the_following_number_of_factors(500)
+puts return_first_triangle_number_with_more_than_the_following_number_of_factors(300)
---
+++ 12/cplusplus/main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+
+using namespace std;
+
+int factors(int);
+
+int main()
+{
+	
+	//cout << factors(500);
+	
+	int howManyFactors = 500;
+	
+	int current_leader_number_of_factors = 0;
+	int current_leader_triangle_number = 0;
+	int current_number_of_factors = 0;
+    
+	int current_natural_number = 1;
+	int number_to_add_to_current_triangle_number = 2;
+	
+	while (current_leader_number_of_factors <= howManyFactors)
+	{
+		//cout << current_natural_number << endl;
+		
+		if(current_natural_number % 1000000 == 0)
+		{
+			cout << current_natural_number << endl;
+		}
+		
+		current_number_of_factors = factors(current_natural_number);
+		
+		if (current_number_of_factors > current_leader_number_of_factors)
+		{
+			current_leader_number_of_factors = current_number_of_factors;
+			current_leader_triangle_number   = current_natural_number;
+			cout << current_natural_number << " has " << current_number_of_factors << endl;
+    	}
+
+		current_natural_number += number_to_add_to_current_triangle_number;
+		
+		number_to_add_to_current_triangle_number += 1;
+		
+	}
+	
+	cout << current_leader_triangle_number << endl;
+	
+	return 0;
+}
+
+int factors(int number)
+{
+	int factors = 2;	
+	double half = number/2.0;
+	
+	for(int i=2; i <= half; ++i)
+	{
+		if(number % i == 0)
+		{
+			//cout << i << endl;
+			factors++;
+		}
+	}
+	
+	return factors;
+	
+}
