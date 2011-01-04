62f8f13f8ca257145786259b481f6fba49dfbadf
William Sullivan <enkrates@gmail.com>
2011-01-04 15:00:37 -0600

    Updated the ruby code for number 12; made it look prettier and also run faster, so the use of C++ is not really necessary

--- 12/ruby/code.rb
+++ 12/ruby/code.rb
@@ -2,44 +2,23 @@
 
 # http://projecteuler.net/index.php?section=problems&id=12
 
-def count_factors_in_a_number number
-  
-  #factors = Array.new
+def count_factors_in_a_number number  
   factors = 2
-  half = number/2.0
-  
-  #(1..(Math.sqrt(number))).each do |potential|
-   (2..half).each do |potential|
-    
-    #puts potential
-    
-    if number % potential == 0
-      
-      puts potential.to_s + " is a factor of " + number.to_s
-      
-      factors += 1
-      
-    end
-    
-  end
-  
-  return factors
-  
+   (2..Math.sqrt(number)).each do |potential|    
+    if number % potential == 0      
+      factors += 2      
+    end    
+  end  
+  return factors  
 end
 
 def return_first_triangle_number_with_more_than_the_following_number_of_factors factors
-  
-  #current_leaders = Array.new
   current_leader_number_of_factors = 0
-  current_leader_triangle_number = 0
-    
+  current_leader_triangle_number = 0    
   current_natural_number = 1
   number_to_add_to_current_triangle_number = 2
   
-  while current_leader_number_of_factors <= factors
-    
-    puts current_natural_number
-    
+  while current_leader_number_of_factors <= factors    
     current_number_of_factors = count_factors_in_a_number(current_natural_number)
     
     if current_number_of_factors > current_leader_number_of_factors
@@ -48,17 +27,11 @@ def return_first_triangle_number_with_more_than_the_following_number_of_factors 
       puts current_natural_number.to_s + " has " + current_number_of_factors.to_s
     end
     
-    current_natural_number += number_to_add_to_current_triangle_number
-    
-    number_to_add_to_current_triangle_number += 1
-    
+    current_natural_number += number_to_add_to_current_triangle_number    
+    number_to_add_to_current_triangle_number += 1    
   end
   
   return current_leader_triangle_number
   
-  
 end
-
-
-
-puts return_first_triangle_number_with_more_than_the_following_number_of_factors(300)
+puts return_first_triangle_number_with_more_than_the_following_number_of_factors(500)
