7a07664681f308ddaff129cb04ae133bc66d39bd
William Sullivan <enkrates@gmail.com>
2012-01-06 15:29:26 -0600

    Added a fourth test in problem 1 to confirm that the actual problem is solved. Also changed some capitalization in variables names in the test

--- 1/ruby/test/test.rb
+++ 1/ruby/test/test.rb
@@ -7,21 +7,26 @@ require '../code.rb'
 class TestOneRb < Test::Unit::TestCase
   
   def setup
-    @E1 = EulerNumber.new(10)
-    @E2 = EulerNumber.new(20)
-    @E3 = EulerNumber.new(35)
+    @e1 = EulerNumber.new(10)
+    @e2 = EulerNumber.new(20)
+    @e3 = EulerNumber.new(35)
+    @e4 = EulerNumber.new(1000)
   end
   
   def test_ten
-    assert_equal(23, @E1.sum_of_natural_multiples_of_three_or_five)
+    assert_equal(23, @e1.sum_of_natural_multiples_of_three_or_five)
   end
   
   def test_twenty
-    assert_equal(78, @E2.sum_of_natural_multiples_of_three_or_five)
+    assert_equal(78, @e2.sum_of_natural_multiples_of_three_or_five)
   end
   
   def test_a_hundred
-    assert_equal(258, @E3.sum_of_natural_multiples_of_three_or_five)
+    assert_equal(258, @e3.sum_of_natural_multiples_of_three_or_five)
+  end
+  
+  def test_under_a_thousand
+    assert_equal(233168, @e4.sum_of_natural_multiples_of_three_or_five)
   end
   
 end
